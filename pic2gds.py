4a2e47094d136a0d51fc43a31c8f5d04fefcad16
TheAnsIs42 <44214674+TheAnsIs42@users.noreply.github.com>
2025-02-15 15:03:49 +0900

    (fix) export svg

--- pic2gds.py
+++ pic2gds.py
@@ -75,7 +75,7 @@ if __name__ == "__main__":
         "test.svg",
         background="white",
         shape_style={(1, 0): {"fill": "black", "stroke": "none"}},
-        scale=1,
+        scaling=1,
         pad=0,
     )
     lib.add(cell, *cell.dependencies(True))
